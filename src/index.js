5aceee8ccbd773891aca15e576fbd812479cb56f
huyphung1602 <huy.phung.sw@gmail.com>
2022-04-12 14:22:37 +0700

    Remove parcel-bundler, use parcel

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import { Elm } from './Main.elm'
 
-var storedState = localStorage.getItem('todo-app-save');
-// console.log("Retrieved state: ", storedState);
+var storedState = localStorage.getItem('midoto-save');
 var startingState = storedState ? storedState : null;
 
 var app = Elm.Main.init({
-  node: document.querySelector('main'),
+  node: document.getElementById('root'),
   flags: startingState,
 });
 
 app.ports.storeTodos.subscribe(function(todos) {
   if (todos.length > 0) {
     var todosJson = JSON.stringify(todos);
-    localStorage.setItem('todo-app-save', todosJson);
+    localStorage.setItem('midoto-save', todosJson);
     // console.log("Saved state: ", todosJson);
   }
 });
